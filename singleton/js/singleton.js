be6c933351f22edca0314a8862ce4c5a85af5693
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-08 17:51:57 -0500

    singleton(js): caso práctico

--- singleton/js/singleton.js
+++ singleton/js/singleton.js
@@ -22,8 +22,46 @@ class Singleton {
     }
 }
 
-const singleton = new Singleton();      // no existe, se crea
-const singleton2 = new Singleton();     // ya existe
-console.log(singleton === singleton2);  // true
+const singleton = new Singleton(); // no existe, se crea
+const singleton2 = new Singleton(); // ya existe
+console.log(singleton === singleton2); // true
 const singleton3 = Singleton.getInstance();
 console.log(singleton3 === singleton2); // true
+
+class WeekDays {
+    daysSp = [
+        "domigo",
+        "lunes",
+        "martes",
+        "miércoles",
+        "jueves",
+        "viernes",
+        "sábado",
+    ];
+    daysEn = [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+    ];
+
+    constructor(lang) {
+        this.lang = lang;
+        // no debe llamarse instance
+        if (WeekDays.instance) {
+            return WeekDays.instance;
+        }
+        WeekDays.instance = this;
+    }
+
+    getDays() {
+        return this.lang === "es" ? this.daysSp : this.daysEn;
+    }
+}
+
+const weekDays = new WeekDays("es");
+const weekDays2 = new WeekDays("en");   // no crea nueva configuración, ignora el parámetro
+console.log(weekDays.getDays(), weekDays2.getDays());
