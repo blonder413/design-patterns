17e8bec5a30894918dc958e5b3335be4da0f0a2c
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-08 16:23:07 -0500

    intro: arrow function

--- intro/js/intro.js
+++ intro/js/intro.js
@@ -23,4 +23,19 @@ function operation(fn, a, b) {
     console.log(fn(a, b));
 }
 
-operation(sumar, 4, 13);
+operation(sumar, 4, 13); // 17
+
+/** Arrow function */
+// Cuando solo hay una línea las llaves sobran
+let fa = () => console.log("Algo");
+fa(); // algo
+
+operation((num1, num2) => num1 * num2, 8, 10); // 80
+operation(
+    (a, b) => {
+        const c = a + b;
+        return c * 2;
+    },
+    4,
+    13
+); // (4+13) * 2 = 34
