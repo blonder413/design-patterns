fe597855e9439780d95f729570d8526c10cc56e3
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-08 17:13:17 -0500

    intro(ts): clases y objetos

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -1,1 +1,15 @@
 console.log("Hola desde ts");
+
+class Drink {
+    private name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+}
+
+const coffe = new Drink("café colombiano");
+console.log(coffe.getName());
